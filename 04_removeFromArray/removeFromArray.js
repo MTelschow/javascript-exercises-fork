9e525e323b0d145941a17ba3cdcdda15111d5919
Mathis Telschow <mathistelschow@gmail.com>
2022-06-20 13:58:14 +0200

    Solve ex-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...others) {
+    for (let item of others){
+        if (array.includes(item)){
+            let location = array.indexOf(item);
+            array.splice(location, 1);
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line

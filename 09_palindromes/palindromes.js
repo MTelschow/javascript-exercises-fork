cf975893f7c0a6ec5bcd9755b88462ba0e4d7843
Mathis Telschow <mathistelschow@gmail.com>
2022-06-20 15:33:26 +0200

    Solve ex-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let result = true;
+    let letters = string.toLowerCase().replace(/\W/g,'');
+    for(let i = 0; i < letters.length/2; i++){
+        if (letters.charAt(i) !== letters.charAt(letters.length - i -1)){
+            result = false;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

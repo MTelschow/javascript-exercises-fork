b1c7b6238c9c884a00d0cfa141852d7a91e40af9
Mathis Telschow <mathistelschow@gmail.com>
2022-06-20 13:17:58 +0200

    Solve ex-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats < 0){return "ERROR"}
+    output = "";
+    for (let i = 0; i < repeats; i++){
+        output = output + string;
+    }
+    return output;
 };
 
 // Do not edit below this line

6a8e32a9b37c468b074cdce4d9e7221165d7cb48
Mathis Telschow <mathistelschow@gmail.com>
2022-06-20 14:36:20 +0200

    Solve ex-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(int1 > int2){
+        let temp = int1;
+        int1 = int2;
+        int2 = temp;
+    }
+    if (typeof(int1) === typeof(int2) &&
+            typeof(int1) ===  "number" &&
+            int1 >= 0){
+        result = 0;
+        for(let i = int1; i <= int2; i++) {
+            result += i;
+        }
+        return result;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line

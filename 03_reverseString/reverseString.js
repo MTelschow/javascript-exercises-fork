57ff294a44507a745fc4017abf741ce372f44703
Mathis Telschow <mathistelschow@gmail.com>
2022-06-20 13:31:43 +0200

    Solve ex-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    for (let i = 0; i < string.length; i++){
+        output += string.slice(string.length-i-1, string.length-i);
+    }
+    return output;
 };
 
 // Do not edit below this line

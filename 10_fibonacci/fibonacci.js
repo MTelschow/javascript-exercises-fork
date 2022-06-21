e2b1997c9316f0c3de7a70c92132a62ca1ff925b
Mathis Telschow <mathistelschow@gmail.com>
2022-06-21 10:16:20 +0200

    Solve ex-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = parseInt(position);
+    if (position < 0) {
+        return "OOPS";
+    }
+    let  a = 0;
+    let b = 1;
+    for (let i=0; i< position; i++){
+        a += b;
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    return a;
 };
 
 // Do not edit below this line

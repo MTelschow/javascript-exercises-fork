6eb0109a87068121857e534e02a3d7f432004f40
Mathis Telschow <mathistelschow@gmail.com>
2022-06-21 11:04:35 +0200

    Solve ex-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    const ordered = persons.sort(function(a, b){
+        const currentYear = 2022;
+        let deathA = (a.yearOfDeath == undefined ? currentYear : a.yearOfDeath);
+        let deathB = (b.yearOfDeath == undefined ? currentYear : b.yearOfDeath);
+
+        let yearsLivedA = deathA - a.yearOfBirth;
+        let yearsLivedB = deathB - b.yearOfBirth;
+        return (yearsLivedA < yearsLivedB ? 1 : -1);
+    })
+
+    return ordered[0];
 };
 
 // Do not edit below this line

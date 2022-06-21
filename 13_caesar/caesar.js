96c42c6eafd0ca95f88454cac24c3812a46ffa23
Mathis Telschow <mathistelschow@gmail.com>
2022-06-21 12:14:08 +0200

    Solve ex-13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,28 @@
-const caesar = function() {
+const caesar = function(text, amount) {
+    while(amount < 0){
+        amount += 26;
+    }
+    let unicode = [];
+    for(let i =0; i < text.length; i++){
+        unicode.push(text.charCodeAt(i));
+    }
 
+    let cyperUnicode = unicode.map(function(code){
+        if(code > 64 && code < 91){
+            return (code%65 + amount)%26 + 65;
+        }
+        if(code > 96 && code < 123){
+            return (code%97 + amount)%26 + 97;
+        }
+        return code;
+    })
+
+    let cyper = cyperUnicode.reduce((cyperText, char) => {
+        return cyperText + String.fromCharCode(char);
+    }, "")
+
+    return cyper;
+    // return cyperUnicode[0];
 };
 
 // Do not edit below this line
